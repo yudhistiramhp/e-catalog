31ace8200838ea5c49fc121a6d6742ab084e41d3
yudhistiramhp <kadekaw09@gmail.com>
2026-08-05 12:17:15 +0800

    fix: update Firebase admin initialization to use cert credentials

--- server/utils/firebase-admin.ts
+++ server/utils/firebase-admin.ts
@@ -1,8 +1,12 @@
-import { applicationDefault, getApps, initializeApp } from 'firebase-admin/app'
+import { cert, getApps, initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
 const app = getApps()[0] ?? initializeApp({
-  credential: applicationDefault(),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
   projectId: process.env.FIREBASE_PROJECT_ID,
 })
 
